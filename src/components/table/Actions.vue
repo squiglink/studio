5d87ea409783278f45eaa5a8ac45cbffaf0a0357
Aleksei Gostev <starawayy@protonmail.com>
2025-01-05 06:25:15 +0500

    Improve the mobile layout

--- src/components/table/Actions.vue
+++ src/components/table/Actions.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex lg:flex-row sm:flex-col gap-3 justify-between p-4 bg-gray-800 rounded-t-lg">
+  <div class="flex flex-col lg:flex-row gap-3 justify-between p-4 bg-gray-800 rounded-t-lg items-center">
     <slot name="search" />
 
-    <div class="flex flex gap-4">
+    <div class="flex gap-4">
       <slot name="actions" />
     </div>
   </div>
