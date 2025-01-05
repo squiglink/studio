d81d35cf3b68895d8fd8b6b605f8180cd8600246
Aleksei Gostev <starawayy@protonmail.com>
2025-01-05 05:54:16 +0500

    Start implementing the responsive layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,10 +3,10 @@ import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
-  <div class="dark bg-gray-900 flex">
-    <Sidebar class="shrink-0" />
+  <div class="dark bg-gray-900">
+    <Sidebar />
 
-    <div class="w-full overflow-x-auto p-4 flex flex-col gap-6">
+    <div class="sm:ml-72 h-screen overflow-x-auto p-4 flex flex-col gap-6">
       <h1 class="text-3xl text-white">
         <slot name="header-text" />
       </h1>
