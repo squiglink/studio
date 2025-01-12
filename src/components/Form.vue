0e1bb90b09a85181270a4081692aa86d19512cad
Aleksei Gostev <starawayy@protonmail.com>
2025-01-12 20:13:27 +0500

    Create `Form` component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+const errors = defineModel<string[]>()
+</script>
+
 <template>
   <div class="flex flex-col gap-4 h-full">
+    <fwb-alert v-if="errors.length > 0" type="danger">
+      <span class="font-medium">Ensure that these requirements are met:</span>
+      <ul class="mt-1.5 ml-4 list-disc list-inside">
+        <li v-for="error in errors" :key="error">{{ error }}</li>
+      </ul>
+    </fwb-alert>
+
     <div class="flex flex-col gap-3 basis-full">
       <slot name="content" />
     </div>
