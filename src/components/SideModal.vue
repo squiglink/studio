42acca83a0983462b04ae8bbf6a890e384ff0c2e
Aleksei Gostev <starawayy@protonmail.com>
2025-01-13 19:10:34 +0500

    Handle clicks outside of the side panel and `escape` keypresses to close the modal

--- src/components/SideModal.vue
+++ src/components/SideModal.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
 const modalShown = defineModel<boolean>()
 
 const closeModal = () => {
   modalShown.value = false
 }
+
+onMounted(() => {
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      closeModal()
+    }
+  })
+
+  window.addEventListener('click', (e) => {
+    if (e.target === document.querySelector('#modal-container')) {
+      closeModal()
+    }
+  })
+})
 </script>
 
 <template>
   <div v-if="modalShown"
-       class="bg-gray-600 bg-opacity-80 absolute top-0 left-0 w-full h-full z-50">
+       id="modal-container"
+       class="bg-gray-600 bg-opacity-80 absolute top-0 left-0 w-full h-full z-50"
+  >
     <div class="w-[596px] bg-gray-900 h-full p-4 flex flex-col gap-3" :class="{ 'animate-left-slide-in': modalShown }">
       <div class="flex justify-between items-center">
         <h2 class="text-base text-gray-400 font-semibold uppercase"><slot name="header-text" /></h2>
