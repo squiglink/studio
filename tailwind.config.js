d2c07fab2dc02f44aca6e929cf73682e5db01632
Aleksei Gostev <starawayy@protonmail.com>
2025-08-12 02:22:47 +0500

    Upgrade to TailwindCSS v4 and Flowbite-vue v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-import flowbitePlugin from 'flowbite/plugin'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
     'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [flowbitePlugin],
+  plugins: [require('flowbite/plugin')],
 }
