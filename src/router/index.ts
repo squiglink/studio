2ce799943c44dc4c78e3efc476691139cc764822
Andrei Zhigalkin <snacks64@proton.me>
2026-04-13 11:18:35 +0500

    Sort routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,68 +4,68 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("../views/databases/DatabasesView.vue"),
-    },
-    {
-      path: "/databases",
-      name: "databases",
-      component: () => import("../views/databases/DatabasesView.vue"),
-    },
-    {
-      path: "/databases/new",
-      name: "newDatabase",
-      component: () => import("../views/databases/NewDatabaseView.vue"),
-    },
-    {
-      path: "/databases/:id/edit",
-      name: "editDatabase",
-      component: () => import("../views/databases/EditDatabaseView.vue"),
-    },
-    {
-      path: "/brands",
-      name: "brands",
-      component: () => import("../views/brands/BrandsView.vue"),
-    },
-    {
-      path: "/brands/new",
-      name: "newBrand",
-      component: () => import("../views/brands/NewBrandView.vue"),
-    },
-    {
-      path: "/brands/:id/edit",
-      name: "editBrand",
-      component: () => import("../views/brands/EditBrandView.vue"),
-    },
-    {
-      path: "/models",
-      name: "models",
-      component: () => import("../views/models/ModelsView.vue"),
-    },
-    {
-      path: "/models/new",
-      name: "newModel",
-      component: () => import("../views/models/NewModelView.vue"),
-    },
-    {
-      path: "/models/:id/edit",
-      name: "editModel",
-      component: () => import("../views/models/EditModelView.vue"),
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: () => import("../views/auth/LoginView.vue"),
-    },
-    {
-      path: "/auth/verify",
-      name: "authVerify",
       component: () => import("../views/auth/AuthVerifyView.vue"),
+      name: "authVerify",
+      path: "/auth/verify",
     },
     {
-      path: "/:pathMatch(.*)*",
+      component: () => import("../views/auth/LoginView.vue"),
+      name: "login",
+      path: "/login",
+    },
+    {
+      component: () => import("../views/brands/BrandsView.vue"),
+      name: "brands",
+      path: "/brands",
+    },
+    {
+      component: () => import("../views/brands/EditBrandView.vue"),
+      name: "editBrand",
+      path: "/brands/:id/edit",
+    },
+    {
+      component: () => import("../views/brands/NewBrandView.vue"),
+      name: "newBrand",
+      path: "/brands/new",
+    },
+    {
+      component: () => import("../views/databases/DatabasesView.vue"),
+      name: "databases",
+      path: "/databases",
+    },
+    {
+      component: () => import("../views/databases/DatabasesView.vue"),
+      name: "home",
+      path: "/",
+    },
+    {
+      component: () => import("../views/databases/EditDatabaseView.vue"),
+      name: "editDatabase",
+      path: "/databases/:id/edit",
+    },
+    {
+      component: () => import("../views/databases/NewDatabaseView.vue"),
+      name: "newDatabase",
+      path: "/databases/new",
+    },
+    {
+      component: () => import("../views/models/EditModelView.vue"),
+      name: "editModel",
+      path: "/models/:id/edit",
+    },
+    {
+      component: () => import("../views/models/ModelsView.vue"),
+      name: "models",
+      path: "/models",
+    },
+    {
+      component: () => import("../views/models/NewModelView.vue"),
+      name: "newModel",
+      path: "/models/new",
+    },
+    {
       component: () => import("../views/PageNotFoundView.vue"),
+      path: "/:pathMatch(.*)*",
     },
   ],
 });
